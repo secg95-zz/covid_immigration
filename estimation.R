be61ee4f69a704708146ce96b3bf432ad500f960
JFCeron <juanfceron@gmail.com>
2021-01-23 15:28:13 -0800

    Inject discretized serial interval dependency into methods that use it

--- estimation.R
+++ estimation.R
@@ -1,16 +1,25 @@
-source("discretize_dist.R")
+'
+Estimators for R given an incidence series. All estimators accept the same
+parameters.
 
-epiestim = function(I, si_dist, si_scale, si_shape) {
-  discrete_si = discretize_dist(
-    si_dist = si_dist,
-    si_shape = si_shape,
-    si_scale = si_scale,
-    n_vals = length(I)
-  )
+I : double
+    Series of incidences.
+discrete_si : double
+  Discretized distribution of the serial interval. discretete_si[i + 1] :=
+  probability of serial interval = i.
+'
+library(EpiEstim)
+
+epiestim = function(I, discrete_si) {
   R_hat = estimate_R(
     incid = I,
     method = "non_parametric_si", # user-specified discrete distribution
     config = make_config(list(si_distr = c(0, discrete_si)))
   )
   return(R_hat)
+}
+
+ekf = function(I, si_dist, si_scale, si_shape) {
+
+  
 }
--- test.R
+++ test.R
@@ -3,6 +3,10 @@ Simulates several epidemics with the same parameters and gathers goodness of fit
 metrics for the available models.
 "
 library(RJSONIO)
+sim = new.env(); source("simulation.R", local=sim)
+est = new.env(); source("estimation.R", local=est)
+source("discretize_dist.R")
+
 OUT_DIR = "results"
 
 config = list(
@@ -15,16 +19,18 @@ config = list(
   immigration_rate = 2,
   R = rep(1.2, 50)
 )
-# Import simulation and estimation modules
-sim = new.env(); source("simulation.R", local=sim)
-est = new.env(); source("estimation.R", local=est)
+# Simulate and estimate R
+discrete_si = discretize_dist(
+  si_dist = config$si_dist,
+  si_shape = config$si_shape,
+  si_scale = config$si_scale,
+  n_vals = config$n_epidemics
+)
 results = NULL
 for (. in 1:config$n_epidemics) {
   # Simulate epidemic
   simulation = sim$simulate_with_immigrants(
-    si_dist = config$si_dist,
-    si_shape = config$si_shape,
-    si_scale = config$si_scale,
+    discrete_si = discrete_si,
     initial_cases = config$initial_cases,
     time_span = config$time_span,
     immigration_rate = config$immigration_rate,
@@ -33,16 +39,20 @@ for (. in 1:config$n_epidemics) {
   # Estimate R using each of the available algorithms
   R_epiestim = est$epiestim(
     I = simulation$I,
-    si_dist = config$si_dist,
-    si_scale = config$si_scale,
-    si_shape = config$si_shape
+    discrete_si = discrete_si
   )$R$`Mean(R)`
   # Collect estimations and goodness of fit measures
-  results = c(results, list(list(
-    I = simulation$I, XI = simulation$XI, R_epiestim = R_epiestim
-  )))
+  results = c(
+    results,
+    list(list(
+      I = simulation$I,
+      XI = simulation$XI,
+      R_epiestim = R_epiestim
+    ))
+  )
 }
 # Store results in disk
+summary = list(parameters = config, results = results)
 dir.create(OUT_DIR, recursive=TRUE)
 timestamp = format(Sys.time(), "%Y%m%d%H%M")
-write(toJSON(results), paste0(OUT_DIR, "/", timestamp, ".json"))
+write(toJSON(summary), paste0(OUT_DIR, "/", timestamp, ".json"))
