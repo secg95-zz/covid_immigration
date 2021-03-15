c45c34866c74a393f6e1f453637c6e6998d7f870
Santiago Cortes <secg95@gmail.com>
2021-03-14 19:59:46 -0500

    refactoring

--- UKF/UKF.R
+++ UKF/UKF.R
@@ -5,6 +5,23 @@ library(nloptr)
 library(reshape2)
 library(np)
 
+create_sample=function(sqrt_P, a_t, m, k){
+  "The is aproblem here, the ones abowe weights more, exponential function! ponerle logs????"
+    sigmas = list()
+    sigmas[[1]] = a_t
+    w = rep(0, 2*m + 1) 
+    for(j in 1:m){
+      P_ti = sqrt_P[,j]
+      x_ti = a_t + sqrt(m + k)*P_ti
+      x_im = a_t - sqrt(m + k)*P_ti
+      sigmas[[j + 1]] = x_ti
+      sigmas[[(m + j) + 1]] = x_im
+      w[1] = k/(k + m)
+      w[j + 1] = 1/(2*(m + k))
+      w[m + j + 1] =  1/(2*(m + k))
+    }
+  return(list(sigmas=sigmas,w=w))
+}
 
 UKF_FS=function(y,x, par1,par2,par3, m=3){
   "Unscentend Kalman filter
@@ -18,7 +35,6 @@ UKF_FS=function(y,x, par1,par2,par3, m=3){
     Initial variance of alpha given y_t.
   par3 : double
     eta's initial variance, as these errors are uncorralted the suggested value for this paramter is: 1"
-  print(y)
   a=as.matrix(par1)
   P=list()
   v=list()
@@ -35,89 +51,78 @@ UKF_FS=function(y,x, par1,par2,par3, m=3){
     a_t=a[,i]
     P_t=P[[i]]
     P_aux = chol(P_t)
-    sigmas = list()
-    x_t0 = a_t
- 
-    sigmas[[1]] = x_t0
-    w = rep(0, 2*m + 1) 
-    for(j in 1:m){
-      P_ti = P_aux[[j]]
-      x_ti = a_t + sqrt(m + k)*P_ti
-      x_im = a_t - sqrt(m + k)*P_ti
-      sigmas[[j + 1]] = x_ti
-      sigmas[[(m + j) + 1]] = x_im
-      w[1] = k/(k + m)
-      w[j + 1] = 1/(2*(m + k))
-      w[m + j + 1] =  1/(2*(m + k))
+    samples_a_t = create_sample(P_aux, a_t, m, k)
+    samples_x[[i]] = samples_a_t$sigmas
+    sigmas = samples_a_t$sigmas
+    w = samples_a_t$w
+    y_t_expected = 0
+    z_t = rep(0, 2*m +1) 
+    for (j in 1:(2*m+1)){
+      xtj = sigmas[[j]]
+      z_tj = exp(xtj[1])*(x[i]+ exp(xtj[2]))
+      print("z_j")
+      print(z_tj)
+      z_t[j] = z_tj
+      y_t_expected = y_t_expected + w[j]*z_tj
     }
 
-    samples_x[[i]] = sigmas
-    y_t_expected = 0
-    for (j in 1:(2*m)+1){
-      xti = sigmas[[j]]
-      z_ti = exp(xti[1])*(x[i]+ exp(xti[2]))
-      y_t_expected = y_t_expected + w[j]*z_ti
-    }
-    print(y_t_expected)
-    P_alphavt = 0
-    for(j in 1:(2*m)+1){
-      xti = sigmas[[j]]
-      z_ti  = exp(xti[1])*(x[i]+ exp(xti[2]))
-      P_alphavt = P_alphavt + w[j]*((xti - a_t)*(z_ti - y_t_expected))
-    }
     P_vvt = 0
-    for(j in 1:(2*m)+1){
-      xti = sigmas[[j]]
-      z_ti  =  exp(xti[1])*(x[i]+ exp(xti[2]))
-      P_vvt = P_vvt + w[j]*((z_ti - y_t_expected)*(z_ti - y_t_expected))
+    P_alphavt = 0
+    for(j in 1:(2*m+1)){
+      xtj = sigmas[[j]]
+      z_tj  = z_t[j]
+      P_alphavt = P_alphavt + w[j]*((xtj - a_t)*(z_tj - y_t_expected))
+      P_vvt = P_vvt + w[j]*((z_tj - y_t_expected)*(z_tj - y_t_expected))
     }
+    
+    print("predictions")
+    print(y[i])
+    print(y_t_expected)
     v_t = y[i] - y_t_expected
     
-    a_tt=a_t+P_alphavt*(1/P_vvt)*v_t
+    a_tt = a_t + P_alphavt*(1/P_vvt)*v_t
     a_tts[[i]] = a_tt
-    print(P_t)
-    print(P_alphavt)
     P_tt=P_t-P_alphavt%*%((1/P_vvt)*t(P_alphavt))
-    print(P_tt)
-    sigmas_n = list()
-    x_t0_n = a_tt
-    sigmas_n[[1]] = x_t0_n
-    w_n = rep(0, 2*m + 1) 
+ 
     P_aux_n = chol(P_tt)
-    for(j in 1:m){
-      P_ti_n = P_aux_n[[j]]
-      x_ti_n = a_tt + sqrt(m + k)*P_ti_n
-      x_im_n = a_tt - sqrt(m + k)*P_ti_n
-      sigmas_n[[j + 1]] = x_ti_n
-      sigmas_n[[(m + j) + 1]] = x_im_n
-      w_n[1] = k/(k + m)
-      w_n[j + 1] = 1/(2*(m + k))
-      w_n[m + j + 1] =  1/(2*(m + k))
-    }
+    samples_a_t1 = create_sample(P_aux_n, a_tt, m, k)
+    sigmas_n = samples_a_t1$sigmas
+    w_n = samples_a_t1$w
+
     a_t1=0
-    for(j in 1:(2*m)+1){
-      xti_n = sigmas_n[[j]]
-      a_t1 = a_t1 + w_n[j]*(Tmat%*%xti_n)
+    for(j in 1:(2*m+1)){
+      xtj_n = sigmas_n[[j]]
+      a_t1 = a_t1 + w_n[j]*(Tmat%*%xtj_n)
     }
     P_t1 = 0
-    for(j in 1:(2*m)+1){
-      xti_n = sigmas_n[[j]]
-      P_t1 =  P_t1 + w_n[j]*((Tmat%*%xti_n - a_t1)%*%t(Tmat%*%xti_n - a_t1)) + w_n[j]*Q
+    for(j in 1:(2*m+1)){
+      xtj_n = sigmas_n[[j]]
+      P_t1 =  P_t1 + w_n[j]*((Tmat%*%xtj_n - a_t1)%*%t(Tmat%*%xtj_n - a_t1)) + w_n[j]*Q
     }
     a=cbind(a,a_t1)
+    print("diferences")
+    print(a_t1 - a_t)
     P[[i+1]]=P_t1
     v[[i]]=v_t
   }
+  print("expected before smoothing")
+  print(a)
+  print(exp(a[1,]))
+  print(x+exp(a[2,]))
+  print(t(y))
   #smoothing
+
   alpha=matrix(0,3,sizeY)
   for(i in rev(2:sizeY)){
     C_t1 = 0
-    sigmas = samples_x[i]
-    for(j in 1:(2*m)+1){
-      xti = sigmas[[j]]
-      C_t1 = C_t1 + w[j]*((xti - a_t)%*%t(Tmat%*%xti - a[i]))
+    a_t1 = a[i]
+    a_t = a[i - 1]
+    sigmas = samples_x[i - 1]
+    for(j in 1:(2*m+1)){
+      xti = sigmas[[1]][[j]]
+      C_t1 = C_t1 + w[j]*((xti - a_t)%*%t(Tmat%*%xti - a_t1))
     }
-    alpha[,i - 1] = a_tts[[i - 1]] + C_t1%*%(P[[i]]^-1)%*%(alpha[,i] - a[i])
+    alpha[,i - 1] = a_tts[[i - 1]] + C_t1%*%(P[[i]]^-1)%*%(alpha[,i] - a_t1)
   }
   #TODO: compute Poisson Maximun likelihood
   logLikelihood=sum(dpois(y,exp(alpha[1,])*(x+exp(alpha[2,])), log=TRUE))
